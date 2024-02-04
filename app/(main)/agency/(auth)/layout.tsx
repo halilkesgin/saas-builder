c15e6a2c0b6c8bcb7ef0c028736d8e492a371a43
Halil <halillkesgin@gmail.com>
2024-02-04 18:03:30 +0300

    created skeleton

---
+++ app/(main)/agency/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface AuthLayoutProps {
+    children: ReactNode;
+}
+
+const AuthLayout = ({
+    children
+}: AuthLayoutProps) => {
+    return (
+        <div className="h-full flex items-center justify-center">
+            {children}
+        </div>
+    )
+}
+
+export default AuthLayout;
---
+++ app/site/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import { Navbar } from "./_components/navbar";
+
+interface SiteLayoutProps {
+    children: ReactNode
+}
+
+const SiteLayout = ({
+    children
+}: SiteLayoutProps) => {
+    return (
+        <div className="h-full">
+            <Navbar />
+            {children}
+        </div>
+    )
+}
+
+export default SiteLayout;
